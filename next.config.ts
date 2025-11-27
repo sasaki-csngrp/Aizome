11adbd9c7f8cf2862cc47d31e2a2cd8ea38a2530
sasaki-csngrp <sasaki@csngrp.co.jp>
2025-11-27 00:10:46 +0000

    fix: Vercelの制約に達したので、画像変換を停止

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    unoptimized: true,
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
